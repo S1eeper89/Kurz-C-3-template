c862134f29f928ba04795a9f8777934033ccbaea
S1eper <hlahulek.jan@gmail.com>
2025-09-30 20:56:57 +0200

    Test commit

--- ToDoList/src/ToDoList.WebApi/Program.cs
+++ ToDoList/src/ToDoList.WebApi/Program.cs
@@ -3,6 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapGet("/test", () => "This is a test.");
-app.MapGet("/cyechitas", () => "Vitej na kurzu Czechitas!");
+app.MapGet("/cyechitas", () => "Vitej na kurzu Czechitas Honzo!");
 app.MapGet("/poydrav/{jmeno}", (string jmeno) => $"Ahoj {jmeno}!");
 app.Run();
