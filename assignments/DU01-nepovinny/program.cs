124b47e93ef200cca54cf638daf8b5c50bea469d
S1eper <hlahulek.jan@gmail.com>
2025-09-30 18:16:42 +0200

    reseni DU 2

---
+++ assignments/DU01-nepovinny/program.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignments.DU01-nepovinny
+{
+    public class program
+    {
+        System.Console.WriteLine("ahoj");
+    }
+}
